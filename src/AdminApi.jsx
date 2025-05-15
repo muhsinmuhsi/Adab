8fa8f769c443dcfc95e90c32fa1a1836a6a2b175
muhzin ck <muhsinmuhsinmuhsi786@gmail.com>
2025-05-15 10:33:55 +0530

    api changed

--- src/AdminApi.jsx
+++ src/AdminApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const adminApi= axios.create({
-    baseURL: "http://localhost:8080/api/admin",
+    baseURL: "https://adab-backend.onrender.com/api/admin",
     withCredentials:true,
 })
 
