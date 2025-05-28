71dc8d846e4d5019e89be277b24a44fc48d57118
muhzin ck <muhsinmuhsinmuhsi786@gmail.com>
2025-05-28 20:02:54 +0530

    admin api edited

--- src/AdminApi.jsx
+++ src/AdminApi.jsx
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const adminApi= axios.create({
-    baseURL:"http://localhost:8080/api/admin" ,
+    baseURL: "https://adab-backend.onrender.com/api/admin",
     withCredentials:true,
 })
-// "https://adab-backend.onrender.com/api/admin"
+
 export default adminApi
